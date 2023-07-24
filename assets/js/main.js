be3890fb6739169e79f0b0de9c66686ff1f4f2cf
Aiden <hideout.bunker@gmail.com>
2023-07-24 20:25:43 +0530

    made design responsive added thankyou popup added email validation

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+// validation js
+const emailInput = document.getElementById("emailAddress");
+const emailValidationText = document.querySelector(".email-validation");
+const emailValue = document.getElementById("userEmail");
+
+const validateEmail = (email) => {
+  const str =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return str.test(String(email).toLowerCase());
+};
+
+const validate = () => {
+  const emailAddress = emailInput.value;
+  if (validateEmail(emailAddress)) {
+    emailValidationText.innerHTML = "valid email address";
+    emailValidationText.classList.remove("not-valid");
+    emailValidationText.classList.add("valid");
+    emailInput.classList.remove("not-valid");
+  } else if (emailAddress === "") {
+    emailValidationText.innerHTML = "";
+    emailInput.classList.remove("not-valid");
+  } else {
+    emailValidationText.innerHTML = "Please enter valid email";
+    emailValidationText.classList.add("not-valid");
+    emailInput.classList.add("not-valid");
+    emailValidationText.classList.remove("valid");
+  }
+};
+
+document.addEventListener("input", validate);
+
+//redirection
+const subBtn = document.getElementById("subscribeBtn");
+
+const sub = () => {
+  window.location.href = "./success.html";
+};
+
+subBtn.addEventListener("click", sub);
---
+++ assets/js/success.js
@@ -0,0 +1,7 @@
+const continueBrowsing = document.getElementById("continueBrowsing");
+
+const waydzo = () => {
+  window.location.href = "https://waydzo.com";
+};
+
+continueBrowsing.addEventListener("click", waydzo);
